4cff31e597a460ca4eb4d6b7b881a0682954d177
Mark hanley <home@markhanley.co.uk>
2023-03-28 23:00:55 +0100

    Remove and simplify  multiple circle body section

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const BOARD_WIDTH = 50; // Width of board in blocks
 const BOARD_HEIGHT = 30; // Height of board in blocks
 const BLOCK_SIZE = 16; // Size of one block in pixels
 const TICK_MS = 250; // Time in milliseconds for each game tick
-const SNAKE_START_LENGTH = 3; // Initial length of snake
+const SNAKE_START_LENGTH = 20; // Initial length of snake
 
 // Create snake Direction enum for readability
 const Direction = Object.freeze({
@@ -18,69 +18,64 @@ const Direction = Object.freeze({
 let board = setUpBoard(BOARD_WIDTH, BOARD_HEIGHT, BLOCK_SIZE);
 document.body.appendChild(board);
 
-let snakeDirection = Direction.up;
+// Set initial direction of snake
+let snakeDirection = Direction.right;
+
+// Build snake (array of SnakePart)
 let snake = setUpSnake(SNAKE_START_LENGTH);
 
 document.body.appendChild(board);
 
-snake.forEach((snakePart) => {
-  board.appendChild(snakePart.svgGroup);
-});
+// Display snake
+displaySnake();
 
 document.addEventListener("keydown", (event) => {
   const key = event.key;
 
-  if (key === "ArrowLeft") x -= 1;
-  if (key === "ArrowRight") x += 1;
-  if (key === "ArrowUp") y -= 1;
-  if (key === "ArrowDown") y += 1;
+  if (key === "ArrowLeft") snakeDirection = Direction.left;
+  if (key === "ArrowRight") snakeDirection = Direction.right;
+  if (key === "ArrowUp") snakeDirection = Direction.up;
+  if (key === "ArrowDown") snakeDirection = Direction.down;
 });
 
+// Set up game loop timer
+setInterval(gameLoop, TICK_MS);
+
 function SnakePart(xPos, yPos, partName, direction) {
-  const circlesPerBlock = 4;
   this.xPos = xPos;
   this.yPos = yPos;
   this.partName = partName;
   this.direction = direction;
+  const radiusEnlargement = 1.4; // Used to scale up circle sections from BLOCK_SIZE of snake so that they overlap
 
   // Create SVG group element
+  // This will enable the development of more complex svg elements for snake parts
   this.svgGroup = document.createElementNS(SVG_NAMESPACE, "g");
   this.svgGroup.setAttributeNS(null, "class", "snake-body-block");
 
-  if (this.partName === "snake-body") {
-    // Create circle elements
-    const radius = BLOCK_SIZE / 2; // Set radius of circle to overlap
+  const radius = (BLOCK_SIZE / 2) * radiusEnlargement; // Set radius of circle to overlap
 
-    // Set increment direction for overlapping circles
-    let increment = getXYIncrement(this.direction);
-
-    for (let i = 0; i < circlesPerBlock; i++) {
-      const x = this.xPos + ((i * increment.x) / circlesPerBlock);
-      const y = this.yPos + ((i * increment.y) / circlesPerBlock);
-      const circle = document.createElementNS(SVG_NAMESPACE, "circle");
-      circle.setAttributeNS(null, "cx", x * BLOCK_SIZE);
-      circle.setAttributeNS(null, "cy", y * BLOCK_SIZE);
-      circle.setAttributeNS(null, "r", radius);
-      circle.setAttributeNS(null, "class", "snake-body"); // Set class name so it can be styled
-      this.svgGroup.appendChild(circle);
-    }
-  }
+  const circle = document.createElementNS(SVG_NAMESPACE, "circle");
+  circle.setAttributeNS(null, "cx", this.xPos);
+  circle.setAttributeNS(null, "cy", this.yPos);
+  circle.setAttributeNS(null, "r", radius);
+  circle.setAttributeNS(null, "class", partName); // Set class name so it can be styled
 
   if (this.partName === "snake-head") {
-    // Create circle elements
-    const radius = (BLOCK_SIZE / 2) * 1.4; // Set radius of circle to overlap
-
-    for (let i = 0; i < circlesPerBlock; i++) {
-      const x = xPos + i * increment.x;
-      const y = yPos + i * increment.y;
-      const circle = document.createElementNS(SVG_NAMESPACE, "circle");
-      circle.setAttributeNS(null, "cx", this.xPos * BLOCK_SIZE);
-      circle.setAttributeNS(null, "cy", this.yPos * BLOCK_SIZE);
-      circle.setAttributeNS(null, "r", this.radius);
-      circle.setAttributeNS(null, "class", this.partName); // Set class name so it can be styled
-      svgGroup.appendChild(circle);
-    }
+    // Add head styling (eyes etc.)
   }
+
+  this.svgGroup.appendChild(circle);
+
+  this.setX = function (x) {
+    this.xPos = x;
+    this.svgGroup.firstElementChild.setAttributeNS(null, "cx", this.xPos);
+  };
+
+  this.setY = function (y) {
+    this.yPos = y;
+    this.svgGroup.firstElementChild.setAttributeNS(null, "cy", this.yPos);
+  };
 }
 
 function setUpBoard() {
@@ -99,13 +94,10 @@ function setUpSnake(length) {
   const increment = getXYIncrement(snakeDirection);
 
   for (let i = 0; i < length; i++) {
-    // const part = i === length - 1 ? "snake-head" : "snake-body";
-    const part = "snake-body";
+    const part = i === length - 1 ? "snake-head" : "snake-body"; // Set the last snake part to be a 'head'
     snake.push(new SnakePart(x, y, part, snakeDirection));
-    console.log(`Before: x=${x} y=${y}`);
-    x += (BLOCK_SIZE * increment.x);
-    y += (BLOCK_SIZE * increment.y);
-    console.log(`After: x=${x} y=${y}`);
+    x += BLOCK_SIZE * increment.x;
+    y += BLOCK_SIZE * increment.y;
   }
   return snake;
 }
@@ -128,7 +120,56 @@ function getXYIncrement(direction) {
   return increment;
 }
 
-// Next steps:
-// Drop circles per block
-// Make circles larger than block
-// Add two eyes for head (in direction of travel) pre-make and transform rotate?
+function moveSnake() {
+  const snakeLength = snake.length;
+
+  // Get reference to old snake head and old snake tail
+  const oldSnakeHead = snake[snakeLength - 1];
+  const oldSnakeTail = snake[0];
+
+  // Clone old snake head into new snake head so that
+  // it can be added to the front of the snake
+
+  // Set class of old snake-head element to be snake-body
+  oldSnakeHead.partName = "snake-body";
+  oldSnakeHead.svgGroup.firstElementChild.setAttributeNS(
+    null,
+    "class",
+    "snake-body"
+  );
+
+  // Get x y coords of new snake section travelling in current direction
+  const increment = getXYIncrement(snakeDirection);
+
+  // Set position of new snake head and display it
+  const oldX = oldSnakeHead.xPos;
+  const oldY = oldSnakeHead.yPos;
+  const newX = oldX + BLOCK_SIZE * increment.x;
+  const newY = oldY + BLOCK_SIZE * increment.y;
+
+  const newSnakeHead = new SnakePart(
+    newX,
+    newY,
+    "snake-head",
+    snakeDirection
+  );
+
+  snake.push(newSnakeHead);
+  board.appendChild(newSnakeHead.svgGroup);
+  console.log(newSnakeHead);
+
+  // Remove end of snake
+  oldSnakeTail.svgGroup.remove();
+  snake.shift();
+}
+
+function growSnake(snake, direction, numberOfBlocks) {}
+
+function displaySnake() {
+  snake.forEach((snakePart) => {
+    board.appendChild(snakePart.svgGroup);
+  });
+}
+function gameLoop() {
+  moveSnake();
+}
